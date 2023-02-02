46e03a342366a0bc683ef396c922279e0c94443e
RaoulBock <anthonyraoulbock01@gmail.com>
2023-02-02 12:47:20 +0200

    front-end

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -6,7 +6,8 @@ import {
   StatusBar,
   FlatList,
   Image,
-  ScrollView
+  ScrollView,
+  RefreshControl
 } from "react-native";
 import React from "react";
 import { COLORS, STATUS_DATA, POST_DATA } from "../Context/settings";
@@ -16,10 +17,24 @@ import SinglePostCard from "../Components/Card/SinglePostCard";
 import BottomNav from "../Components/Nav/BottomNav";
 
 const HomeScreen = () => {
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
+
   const [isLoading, setIsLoading] = React.useState(false);
   return (
     <View style={styles.outline}>
-      <ScrollView>
+      <ScrollView
+        contentContainerStyle={styles.scrollView}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      >
         <HomeNav />
         <View style={{ marginVertical: 5, marginBottom: 20 }}>
           <ScrollView horizontal showsHorizontalScrollIndicator={false}>
