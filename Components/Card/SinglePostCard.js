b5c8b6462999e0cc2424c5f53b59e6fababc594f
RaoulBock <anthonyraoulbock01@gmail.com>
2023-02-02 11:17:11 +0200

    front-end

--- Components/Card/SinglePostCard.js
+++ Components/Card/SinglePostCard.js
@@ -5,16 +5,30 @@ import {
   TouchableOpacity,
   View,
   Dimensions,
+  Alert
 } from "react-native";
 import { Ionicons } from "react-native-vector-icons";
 import React from "react";
 import { APP_ICON, COLORS } from "../../Context/settings";
+import DoubleClick from "react-native-double-click";
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
 const SinglePostCard = ({ item }) => {
   const [isLiked, setIsLiked] = React.useState(false);
+
+  const [clickCount, setClickCount] = React.useState(0);
+
+  const handleClick = () => {
+    setClickCount(clickCount + 1);
+
+    if (clickCount === 1) {
+      Alert.alert("This is awesome \n Double tap succeed");
+    } else {
+      setClickCount(0);
+    }
+  };
 
   return (
     <View style={styles.outline}>
@@ -33,21 +47,34 @@ const SinglePostCard = ({ item }) => {
           <Text>{APP_ICON.ELLIPSIS}</Text>
         </TouchableOpacity>
       </View>
-      <Image source={{ uri: item.post }} style={styles.post} />
+      <DoubleClick onPress={handleClick}>
+        <Image source={{ uri: item.post }} style={styles.post} />
+      </DoubleClick>
 
       <View style={[styles.grid, styles.sectionGrid]}>
         <View style={styles.grid}>
-          <Ionicons
-            name={"ios-heart-outline"}
-            style={isLiked ? styles.isLiked : styles.icon}
-          />
-          <Ionicons
-            name={"ios-chatbubble-outline"}
-            style={[styles.icon, { marginHorizontal: 20, fontSize: 30 }]}
-          />
-          <Ionicons name={"ios-share-outline"} style={styles.icon} />
+          {isLiked ? (
+            <TouchableOpacity onPress={() => setIsLiked(!isLiked)}>
+              <Ionicons name={"ios-heart"} style={styles.isLiked} />
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity onPress={() => setIsLiked(!isLiked)}>
+              <Ionicons name={"ios-heart-outline"} style={styles.icon} />
+            </TouchableOpacity>
+          )}
+          <TouchableOpacity>
+            <Ionicons
+              name={"ios-chatbubble-outline"}
+              style={[styles.icon, { marginHorizontal: 20, fontSize: 30 }]}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Ionicons name={"ios-share-outline"} style={styles.icon} />
+          </TouchableOpacity>
         </View>
-        <Ionicons name={"ios-bookmark-outline"} style={styles.icon} />
+        <TouchableOpacity>
+          <Ionicons name={"ios-bookmark-outline"} style={styles.icon} />
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -59,7 +86,7 @@ const styles = StyleSheet.create({
   outline: {
     // borderTopWidth: 2,
     // borderTopColor: "#2c2e34",
-    marginTop: 7,
+    marginTop: 7
   },
   header: {
     flexDirection: "row",
@@ -67,43 +94,48 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     marginVertical: 10,
     alignItems: "center",
-    marginVertical: 20,
+    marginVertical: 20
   },
   post: {
     width: windowWidth,
     height: 400,
-    resizeMode: "stretch",
+    resizeMode: "stretch"
   },
   profile_image: {
     width: 60,
     height: 60,
-    borderRadius: 50,
+    borderRadius: 50
   },
   grid: {
-    flexDirection: "row",
+    flexDirection: "row"
   },
   userInfo: {
-    paddingHorizontal: 10,
+    paddingHorizontal: 10
   },
   text: {
     color: COLORS.TITLE_TEXT_COLOR,
     fontWeight: "700",
-    fontSize: 20,
+    fontSize: 20
   },
   time: {
     fontSize: 15,
     fontWeight: "500",
-    color: "#6d7e87",
+    color: "#6d7e87"
   },
   sectionGrid: {
     alignItems: "center",
     justifyContent: "space-between",
     marginHorizontal: 10,
-    marginVertical: 10,
+    marginVertical: 10
   },
   icon: {
     color: "white",
     fontWeight: 700,
-    fontSize: 32,
+    fontSize: 32
   },
+  isLiked: {
+    color: "#f66",
+    fontWeight: 700,
+    fontSize: 32
+  }
 });
